22e8217093b32668d60432f38ce73fb14ce3960b
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-24 08:20:25 +1100

    hello world script added

--- 04-variables.sh
+++ 04-variables.sh
@@ -7,6 +7,6 @@ NUMBER2=$2
 
 # I need to run a command inside shell to add these 2 numbers
 
-SUM=$((NUMBER1+NUMBER2))
+SUbtract=$((NUMBER1-NUMBER2))
 
-echo "Addition of 2 numbers is: $SUM"
+echo "Subtraction of 2 numbers is: $Subtract"
