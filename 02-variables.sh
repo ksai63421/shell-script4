28915eb466a870847ebbf0dbd1ea28b2f1825aab
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-24 08:10:51 +1100

    hello world script added

---
+++ 02-variables.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DATE=$(date) #shell script run the command inside dollar and braces and stores the output into variable called date
+echo "Hello this script is executed at timestamp: $DATE"
