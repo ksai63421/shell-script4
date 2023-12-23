2dbedc14049c4a587564dfaac182ebc376dc07d8
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-24 08:01:34 +1100

    hello world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,11 @@
 #!/bib/bash
 
 echo "Hello world, I am learning devops"
+
+echo "Ramesh: hi suresh, good morning"
+
+echo "Suresh: good morning Ramesh"
+
+echo "how are you Suresh"
+
+echo "I am good , how are you Ramesh"
