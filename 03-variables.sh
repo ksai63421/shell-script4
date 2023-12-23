00a7ec70bef39e60036ce7a5ffdf248c11b01e9c
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-24 08:14:23 +1100

    hello world script added

---
+++ 03-variables.sh
@@ -0,0 +1,13 @@
+#!/bib/bash
+
+echo "Hello world, I am learning devops"
+PERSON1=$1 # no spaces here # this is the first value from command line
+PERSON2=$2 # this is the second value from command line
+
+echo "$PERSON1: hi $PERSON2, good morning"
+
+echo "$PERSON2: good morning $PERSON1"
+
+echo "how are you $PERSON2"
+
+echo "I am good , how are you $PERSON1"
