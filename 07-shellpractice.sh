a0875818889bbeea10f34018e5541bbc20c59cbe
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-24 08:50:29 +1100

    hello world script added

--- 07-shellpractice.sh
+++ 07-shellpractice.sh
@@ -1,6 +1,25 @@
-#!/bin/bash
+
 
 # example of using arguments to a script
-echo "My first name is $1"
-echo "My surname is $2"
-echo "Total number of arguments is $#" 
+
+
+# echo "My first name is $1"
+# echo "My surname is $2"
+# echo "Total number of arguments is $#" 
+
+#!/bin/bash
+
+# Assign two numbers to variables
+num1=5
+num2=10
+
+# Compare the numbers using if-else statements
+if [ $num1 -gt $num2 ]
+then
+  echo "$num1 is greater than $num2"
+elif [ $num1 -lt $num2 ]
+then
+  echo "$num1 is less than $num2"
+else
+  echo "$num1 is equal to $num2"
+fi
