6b47570b172d321328a2738f71da539afa235d4c
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-26 07:31:33 +1100

    hello world script added

--- 08.install-git.sh
+++ 08.install-git.sh
@@ -7,8 +7,8 @@ USERID=$(id -u)
 if[ $USERID -ne 0 ]
 then
   echo "ERROR:: Please run this script with root access"
+  exit 1 to 127
 #   else 
 # echo "INFO:: You are not root user"
 fi
-
 yum install git -y
