f4178a6e1667b49a7def0a28a06dc1d9454da20b
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-26 07:37:03 +1100

    hello world script added

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -3,9 +3,10 @@
 # out program goal is to install mysql
 
 USERID=$(id -u)
-if[ $USERID -ne 0 ]
+
+if [ $USERID -ne 0 ]
  echo "ERROR:: Please run this script with root access"
   exit 1 
 fi
 
-yum install git -y
+yum install mysql -y
