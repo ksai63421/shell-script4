ae09c3d2c3b982617258fc7eb8a0ba8cac50fa71
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-27 08:15:42 +1100

    hello world script added

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-# # all argunments are in $@
-# for i in $@
-# do
-#   yum install $i -y
-
-# done 
-
-# task is improvements log files implement colocrs implements validations
-#implements functions
-# implements
-
-# for i in {2..10}
-
-# do 
-#  echo "output: $i"
-
-#  done 
-
-# max=10
-
-# for i 
-# do
-#  echo "output: $i"
-#  done
-
 for i in 1 2 3 4 5
 do
 if [ condition ]
