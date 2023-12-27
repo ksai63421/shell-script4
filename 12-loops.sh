0cee887a1692cbe392ba252c4e547f7aa9b09ef0
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-27 17:04:22 +1100

    hello world script added

--- 12-loops.sh
+++ 12-loops.sh
@@ -6,9 +6,9 @@ N="\e[0m"
 
 if [ $USERID -ne 0 ];
 then
-  echo -e "$R"ERROR: Please run this script with root access $N"
+  echo -e "$R ERROR:: Please run this script with root access $N"
   exit 1
-  fi
+fi
 
 
 
