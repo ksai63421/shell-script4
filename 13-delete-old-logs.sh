79112c811c36c578251a210d83448231446d86e4
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-28 07:34:21 +1100

    hello world script added

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#find /var/log -name "*.log" -type f -mtime +13
+#find /var/log -name "*.log" -type f -mtime +10
 
 APP_LOGS_DIR=/home/centos/app-logs
 
@@ -10,7 +10,7 @@ LOGSDIR=/home/centos/shellscript-logs
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 
-FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +13)
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +12)
 echo "$FILES_TO_DELETE"
 
 while read line
@@ -20,3 +20,12 @@ do
 
 done <<< $FILES_TO_DELETE
 
+
+
+# we need to delete lof files more than 12 days
+
+#only .log files should be deleted, dont delete any other files
+# got to the folder
+# get all the log files with extension of .log
+# check the date
+#if date is more than 2 weeks old date
