880412b358cb6c85eebaf3534113016fdfc23a3b
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-28 22:30:14 +1100

    hello world script added

--- 14-disk-usage.sh
+++ 14-disk-usage.sh
@@ -42,9 +42,18 @@ DISK_USAGE_THRESHOLD=1
 
 while IFS= read line
 do
-  echo "Output: $line"
+# this command will give you usage in number format for comparison 
+  usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+  # this command will give us partition 
+  partition=$(echo $line | awk '{print $1}')
+  # now we need to check whether it is more than thereshold or not
+  if [ $usage -gt $DISK_USAGE_THRESHOLD];
+  then 
+      message+="HIGH DISK USAGE ON $partition: $usage"
+    fi
+done <<< $DISK_USAGE
 
-  done <<< $DISK_USAGE
+echo "message: $message"
 
 
 
