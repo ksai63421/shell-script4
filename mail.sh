7bbbb2503ff9582519d73956c21dd1945622981d
saikrishna94101 <saikrishna94101@gmail.com>
2023-12-29 00:04:33 +1100

    hello world script added

--- mail.sh
+++ mail.sh
@@ -4,7 +4,8 @@
 # mail.sh TO_ADDRESS SUBJECT BODY TEAM_NAME ALERT_TYPE
 TO_ADDRESS=$1
 SUBJECT=$2
-BODY=$3
+BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
+echo "escaped content: $BODY"
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
